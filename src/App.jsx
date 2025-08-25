a1d25be99e4fcdc454cc94ae29557fe4b57a2e73
Ashley Davila <ashleyldavila@gmail.com>
2025-08-25 15:37:20 -0400

    Finished About Us page

--- src/App.jsx
+++ src/App.jsx
@@ -50,28 +50,3 @@ function App() {
 }
 
 export default App;
-
-////////////////////
-
-// import './App.css';
-// import NavBar from './components/NavBar';
-// import Hero from './components/Hero';
-// import MiniContent from './content/MiniContent';
-// import { BrowserRouter } from 'react-router-dom';
-// import Listings from './content/Listings';
-// import Footer from './components/Footer'
-// import AboutUs from './content/AboutUs';
-
-// function App() {
-//   return (
-//     <BrowserRouter>
-//       <NavBar />
-//       <Hero />
-//       <MiniContent />
-//       <Listings />
-//       <Footer />
-//     </BrowserRouter>
-//   );
-// }
-
-// export default App;
